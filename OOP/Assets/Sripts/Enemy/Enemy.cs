9589535c0e7e5607445cb096321def8cd9bcb53f
denmmm120 <denmmm120@gmail.com>
2025-10-05 20:46:00 +0300

    Create EnemyBase and renewal Enemy

--- OOP/Assets/Sripts/Enemy/Enemy.cs
+++ OOP/Assets/Sripts/Enemy/Enemy.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour, IDamagable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Stats")]
+    public int maxHealth = 100;
+    public float Speed = 3.5f;
+
+
+    [Header("Target")]
+    public float sightRange = 8f;
+    public float attackRange = 1.5f;
+
+
+    [Header("Patrol")]
+    public float radiusPatrol = 6f;
+    public float timePatrol = 2f;
+    
 }
