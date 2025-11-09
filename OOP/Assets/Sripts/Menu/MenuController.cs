d9f5d1e06bdc5285b7ecdca794421818e566b880
dimamironuk <dimamironuk2019@gmail.com>
2025-11-09 08:57:18 +0200

    #5 - Доробив панель продавця

--- OOP/Assets/Sripts/Menu/MenuController.cs
+++ OOP/Assets/Sripts/Menu/MenuController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,7 +16,28 @@ public class MenuController : MonoBehaviour
         _sellerPanel.SetActive(false);
         _menuPauseButton.SetActive(true);
     }
+    public void ChooseProductInfo(int index)
+    {
+        SellerController seller = FindObjectOfType<SellerController>();
+        Product chooseProduct = seller.GetProduct(index);
 
+        Image imageChooseProduct = GameObject.Find("I_ChoiceProduct")?.GetComponent<Image>();
+        TextMeshProUGUI nameChooseProduct = GameObject.Find("T_NameChoiceProduct")?.GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI priceChooseProduct = GameObject.Find("T_PriceChoiceProduct")?.GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI infoChooseProduct = GameObject.Find("T_InfoChoiceProduct")?.GetComponent<TextMeshProUGUI>();
+        if (chooseProduct != null) { 
+            imageChooseProduct.sprite = chooseProduct.GetImage();
+            nameChooseProduct.text = chooseProduct.GetName();
+            priceChooseProduct.text = "Price: "+chooseProduct.GetPrice().ToString();
+            infoChooseProduct.text = chooseProduct.GetDescription();
+        }
+        else
+        {
+            nameChooseProduct.text = "None";
+            priceChooseProduct.text = "Price: 0";
+            infoChooseProduct.text = "None";
+        }
+    }
     //Menu Pause
     public void OpenMenuPausePanel()
     {
